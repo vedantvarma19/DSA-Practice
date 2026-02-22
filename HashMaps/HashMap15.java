fd739e56338c11768311cb8cc10a49349f601b3b
Vedant Varma <vedantvarma617@gmail.com>
2026-02-23 00:44:47 +0530

    Solved Hashmap problem on leetcode #49 : Group Anagrams

---
+++ HashMaps/HashMap15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class HashMap15 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            char[] ca = str.toCharArray();
+
+            Arrays.sort(ca);
+
+            String key = new String(ca);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
