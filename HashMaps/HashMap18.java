584417ee5c57ca1000863a0a5ac6bf008960b4f3
Vedant Varma <vedantvarma617@gmail.com>
2026-02-27 01:14:44 +0530

    Solved leetcode problem no 13 : Roman to Integer using HashMap concept

---
+++ HashMaps/HashMap18.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+class HashMap18{
+
+    // Leetcode : https://leetcode.com/problems/roman-to-integer/ 
+    
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>(); 
+        map.put('I',1); 
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int result = 0 ; 
+
+        for(int i=0; i<s.length(); i++){ 
+            int current = map.get(s.charAt(i));
+            if(i<s.length()-1 && current < map.get(s.charAt(i+1))){ 
+                result -= current ; 
+            } else { 
+                result += current ; 
+            }
+            }
+            return result ; 
+        }   
+        
+    }
