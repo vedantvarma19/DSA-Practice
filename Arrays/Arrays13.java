e00c62c22609d319999c7a5796d1dc1b0f3539f6
Vedant Varma <vedantvarma617@gmail.com>
2026-03-12 12:54:18 +0530

    Solved leetcode problem no 941 : Valid Mountain Array

---
+++ Arrays/Arrays13.java
@@ -0,0 +1,25 @@
+public class Arrays13 {
+
+    // https://leetcode.com/problems/valid-mountain-array/description/
+    
+    public boolean validMountainArray(int[] arr) {
+
+        int i = 0 ; 
+        int n = arr.length ; 
+
+        while ( i + 1 < n && arr[i] < arr[i+1]){ 
+            i++ ; 
+        }
+
+        if ( i == 0 || i == n-1){ 
+            return false ; 
+        }
+        
+        while ( i+1 < n && arr[i] > arr[i+1]){ 
+            i++;
+        }
+        return i == n-1; 
+    }
+}
+    
+
