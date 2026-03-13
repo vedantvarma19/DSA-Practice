f36e9ea3645fe175c6471e7c6a666a226741efcc
Vedant Varma <vedantvarma617@gmail.com>
2026-03-13 07:24:31 +0530

    Solved Leetcode problem no 905 : Sort array by parity using two pointer approch

---
+++ Arrays/Arrays15.java
@@ -0,0 +1,29 @@
+public class Arrays15 {
+
+// https://leetcode.com/problems/sort-array-by-parity/ 
+
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            if (nums[left] % 2 > nums[right] % 2) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+
+            if (nums[left] % 2 == 0) {
+                left++;
+            }
+
+            if (nums[right] % 2 == 1) {
+                right--;
+            }
+        }
+
+        return nums;
+    }
+}
+    
